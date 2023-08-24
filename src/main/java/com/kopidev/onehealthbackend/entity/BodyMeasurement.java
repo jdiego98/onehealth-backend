07f03cfaef385efcad29d364e608d8f462fabec9
jramirezo <98234695+jramicr@users.noreply.github.com>
2023-08-24 00:52:31 -0600

    Update BodyMeasurement.java

--- src/main/java/com/kopidev/onehealthbackend/entity/BodyMeasurement.java
+++ src/main/java/com/kopidev/onehealthbackend/entity/BodyMeasurement.java
@@ -15,20 +15,20 @@ public class BodyMeasurement {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private @Id long id;
-    private long weight;
-    private long height;
-    private long bmr;
-    private long bodyFatPercentage;
-    private long bodyFatMass;
-    private long asm;
-    private long bmi;
-    private long tricepSkinfold;
-    private long bicepsSkinfold;
-    private long subscapulaSkinfold;
-    private long suprailiacSkinfold;
-    private long chestSkinfold;
-    private long thighSkinfold;
-    private long legSkinfold;
+    private double weight;
+    private double height;
+    private double bmr;
+    private double bodyFatPercentage;
+    private double bodyFatMass;
+    private double asm;
+    private double bmi;
+    private double tricepSkinfold;
+    private double bicepsSkinfold;
+    private double subscapulaSkinfold;
+    private double suprailiacSkinfold;
+    private double chestSkinfold;
+    private double thighSkinfold;
+    private double legSkinfold;
     private long measurementDate;
     private long userId;
 }
